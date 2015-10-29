c80dcd6de51a96b5a85cf9a8c0a5e16944a8670a
LnAlex <contact@alexandrelion.com>
2015-10-29 16:57:21 +0100

    :white_check_mark: Add test for getLineType

--- main_test.go
+++ main_test.go
@@ -14,4 +14,29 @@ func TestReadLines(t *testing.T) {
 	assert.Equal("{t: Hello world}", lines[0], "they should be equal")
 	assert.Equal("", lines[4], "they should be equal")
 	assert.Equal("[G]Lorem ipsum dolor sit amet, [D/F#]consectetur adipiscing elit. Don[Em]ec a diam lectus.", lines[5], "they should be equal")
+
+}
+
+func TestGetLineType(t *testing.T) {
+
+	assert := assert.New(t)
+	lines, err := readLines("./test.chordpro")
+
+	assert.Nil(err)
+
+	// Testing title type
+	titleType, _, _ := getLineType(lines[0])
+	assert.Equal("title", titleType, "they should have the same type")
+
+	// Testing subtitle type
+	stType, _, _ := getLineType(lines[1])
+	assert.Equal("subtitle", stType, "they should have the same type")
+
+	// Testing comment type
+	commentType, _, _ := getLineType(lines[3])
+	assert.Equal("comment", commentType, "they should have the same type")
+
+	// Testing verse type
+	verseType, _, _ := getLineType(lines[5])
+	assert.Equal("verse", verseType, "they should have the same type")
 }
